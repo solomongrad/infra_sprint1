25505a3198e024cf38b07b57e99ed39d5217fe0a
solomongrad <solomongrad@gmail.com>
2024-08-29 18:48:14 +0000

    secret key

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,9 +1,11 @@
 import os
 from pathlib import Path
 
+os.environ['SECRET_KEY'] = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = False
 
